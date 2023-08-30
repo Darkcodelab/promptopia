84af4f84b3aaf441102d10e8db2e81c754bea275
Darkcodelab <deepanchakravarthi434@gmail.com>
2023-08-30 22:14:13 +0530

    Fixed typo in /api/prompt route

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -6,7 +6,7 @@ export const GET = async (req) => {
     await connectToDB();
     const prompts = await Prompt.find({}).populate("creator");
 
-    return new Response(JSON.stringify(prompts), { statue: 200 });
+    return new Response(JSON.stringify(prompts), { status: 200 });
   } catch (error) {
     return new Response("Failed to fetch", { status: 500 });
   }
